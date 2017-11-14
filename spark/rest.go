f3d495f5923085c3577480517e43454cf22ab48e
Ryan Roden-Corrent <ryan@rcorre.net>
2017-11-13 22:18:17 -0500

    Implement REST client interface

---
+++ spark/rest.go
@@ -0,0 +1,85 @@
+package spark
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type HTTPClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+type RESTClient interface {
+	Get(url string, params map[string]string, out interface{}) error
+}
+
+type restClient struct {
+	http  HTTPClient
+	url   string
+	token string
+}
+
+func NewRESTClient(url, token string) RESTClient {
+	if !strings.HasSuffix(url, "/") {
+		url += "/"
+	}
+	return &restClient{
+		http:  http.DefaultClient,
+		url:   url,
+		token: token,
+	}
+}
+
+// err forms an error message with the spark token masked
+func (c *restClient) err(format string, args ...interface{}) error {
+	str := fmt.Sprintf(format, args...)
+	str = strings.Replace(str, c.token, "REDACTED", -1)
+	return errors.New(str)
+}
+
+// do performs a request forms an error message with the spark token masked
+func (c *restClient) do(req *http.Request, out interface{}) error {
+	req.Header.Set("Authorization", "Bearer "+c.token)
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return c.err("Request %+v failed: %v", req, err)
+	}
+	if resp.StatusCode < 200 || resp.StatusCode > 300 {
+		return c.err("Request %+v had an error response: %+v", req, resp)
+	}
+
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return c.err("Request %+v could not read body from %+v: %v", req, resp, err)
+	}
+
+	if err = json.Unmarshal(bytes, &out); err != nil {
+		return c.err("Could not unmarshal %s into %+v: %v", bytes, out, err)
+	}
+	return nil
+}
+
+func (c *restClient) Get(path string, params map[string]string, out interface{}) error {
+	query := url.Values{}
+	for k, v := range params {
+		query.Set(k, v)
+	}
+
+	url, err := url.Parse(c.url + path)
+	if err != nil {
+		return err
+	}
+	url.RawQuery = query.Encode()
+
+	req, err := http.NewRequest("GET", url.String(), nil)
+	if err != nil {
+		return err
+	}
+	return c.do(req, out)
+}
---
+++ spark/rest_test.go
@@ -0,0 +1,75 @@
+package spark
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type RESTTestSuite struct {
+	suite.Suite
+}
+
+func TestRESTTestSuite(t *testing.T) {
+	suite.Run(t, new(RESTTestSuite))
+}
+
+type HTTPClientMock struct {
+	mock.Mock
+}
+
+func (m *HTTPClientMock) Do(req *http.Request) (*http.Response, error) {
+	args := m.Called(req)
+	return args.Get(0).(*http.Response), args.Error(1)
+}
+
+func (suite *RESTTestSuite) TestGet() {
+	test := func(params map[string]string, expectedURL string) {
+		suite.T().Helper()
+
+		httpClient := &HTTPClientMock{}
+		expectedRequest, _ := http.NewRequest(
+			"GET",
+			expectedURL,
+			nil,
+		)
+		expectedRequest.Header.Set("Authorization", "Bearer soopersecret")
+		httpClient.On(
+			"Do",
+			expectedRequest,
+		).Return(
+			&http.Response{
+				StatusCode: 200,
+				Body: ioutil.NopCloser(
+					bytes.NewBuffer([]byte(
+						`{"foo": "bar"}`,
+					)),
+				),
+			},
+			nil,
+		)
+		rest := restClient{
+			http:  httpClient,
+			url:   "http://example.com/",
+			token: "soopersecret",
+		}
+
+		var out struct {
+			Foo string
+		}
+		err := rest.Get("resource/path", params, &out)
+		suite.Nil(err)
+		suite.Equal(out.Foo, "bar")
+	}
+
+	test(map[string]string{},
+		"http://example.com/resource/path")
+	test(map[string]string{"foo": "bar"},
+		"http://example.com/resource/path?foo=bar")
+	test(map[string]string{"foo": "bar", "biz": "baz"},
+		"http://example.com/resource/path?biz=baz&foo=bar")
+}
