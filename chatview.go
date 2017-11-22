82f9e44313376b85d99c55e88d76dd6d5a8caa2c
Ryan Roden-Corrent <ryan@rcorre.net>
2017-11-22 15:00:45 -0500

    Move chatview logic into functions

--- chatview.go
+++ chatview.go
@@ -28,33 +28,42 @@ type State struct {
 func (*chatView) Render(g *gocui.Gui, state *State) error {
 	roomBarWidth := 30
 	maxX, maxY := g.Size()
-	if v, err := g.SetView("chat", roomBarWidth, 0, maxX, maxY); err != nil && err != gocui.ErrUnknownView {
+	chatView, err := g.SetView("chat", roomBarWidth, 0, maxX, maxY)
+	if err != nil && err != gocui.ErrUnknownView {
 		return err
-	} else {
-		w := new(tabwriter.Writer)
-		v.Clear()
-		w.Init(v, 8, 8, 1, ' ', 0)
-		for _, m := range state.Messages {
-			fmt.Fprintf(w, "%s\t| %s\t| %s\n", m.Time, m.Sender, m.Text)
-		}
-		if err := w.Flush(); err != nil {
-			return err
-		}
 	}
-	if v, err := g.SetView("rooms", 0, 0, roomBarWidth, maxY); err != nil && err != gocui.ErrUnknownView {
+	if err := drawMessages(chatView, state.Messages); err != nil {
 		return err
-	} else {
-		v.Clear()
-		for i, r := range state.Rooms {
-			title := r.Title()
-			if i == state.RoomIdx {
-				title = ansi.Color(r.Title(), "white+b")
-			}
-			fmt.Fprintf(v, "%s\n", title)
-		}
 	}
 
+	roomView, err := g.SetView("rooms", 0, 0, roomBarWidth, maxY)
+	if err != nil && err != gocui.ErrUnknownView {
+		return err
+	}
+	drawRooms(roomView, state.Rooms, state.RoomIdx)
+
 	return nil
+}
+
+func drawRooms(v *gocui.View, rooms []Room, current int) {
+	v.Clear()
+	for i, r := range rooms {
+		title := r.Title()
+		if i == current {
+			title = ansi.Color(r.Title(), "white+b")
+		}
+		fmt.Fprintf(v, "%s\n", title)
+	}
+}
+
+func drawMessages(v *gocui.View, messages []Message) error {
+	w := new(tabwriter.Writer)
+	v.Clear()
+	w.Init(v, 8, 8, 1, ' ', 0)
+	for _, m := range messages {
+		fmt.Fprintf(w, "%s\t| %s\t| %s\n", m.Time, m.Sender, m.Text)
+	}
+	return w.Flush()
 }
 
 func (*chatView) Scroll(g *gocui.Gui, mult float64) error {
